d1c56e12020ba2f5e3b56225092d044e5790a101
magicstf <1399581534@qq.com>
2019-08-03 13:28:12 +0800

    welcome

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import VueRouter from 'vue-router'
 import Vue from 'vue'
 import Login from '@/views/login'
 import Home from '@/views/home'
+import WelCome from '@/views/welcome'
 Vue.use(VueRouter)
 const router = new VueRouter({
   // 路由规则配置
@@ -9,7 +10,14 @@ const router = new VueRouter({
     { path: '/login', name: 'login', component: Login },
     {
       path: '/',
-      component: Home
+      component: Home,
+      children: [
+        {
+          path: '/',
+          name: 'welcome',
+          component: WelCome
+        }
+      ]
     }
   ]
 })

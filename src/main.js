400eded810e10d01d78a75f062b8146c82cac954
magicstf <1399581534@qq.com>
2019-08-03 13:15:16 +0800

    home

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import router from '@/router'
+import axios from 'axios'
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+Vue.prototype.$http = axios
 new Vue({
   router,
   render: h => h(App)
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,16 @@
 import VueRouter from 'vue-router'
 import Vue from 'vue'
 import Login from '@/views/login'
+import Home from '@/views/home'
 Vue.use(VueRouter)
 const router = new VueRouter({
   // 路由规则配置
   routes: [
-    { path: '/login', name: 'login', component: Login }
+    { path: '/login', name: 'login', component: Login },
+    {
+      path: '/',
+      component: Home
+    }
   ]
 })
 
